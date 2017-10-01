447b04573d9d1cbc4e92c3e5f4e9a9eb3f3f78b4
Susan Fowler <infopro94@gmail.com>
2017-10-01 17:23:29 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -58,8 +58,7 @@ class MusicLibraryController
      artist = find_by_name(name)
       if artist != nil
         songs = artist.songs.sort_by {|s| s.name}
-        artist.songs.each_with_index {|s, i| puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-       puts "#{index+1}, #{song.name}"#prints all songs by a particular artist in a numbered, alphabetized list
+        artist.songs.each_with_index {|s, i| puts "#{index + 1}. - #{song.name}"} #prints all songs by a particular artist in a numbered, alphabetized list       
       #does nothing if no matching artist is found
       end
     # end

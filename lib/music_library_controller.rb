804845b01384cd8332dbd4f4937c1f535faf96bd
Susan Fowler <infopro94@gmail.com>
2017-10-01 15:40:37 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -54,7 +54,8 @@ class MusicLibraryController
   def list_songs_by_artist
     input = ""
     puts "Please enter the name of an artist:" #prompts user to enter an artist
-    input = gets.strip #accepts user input 
+    binding.pry
+    input = gets.strip #accepts user input
      if find_by_name(name) == artist.name
        puts "#{index+1}, #{song.name}"#prints all songs by a particular artist in a numbered, alphabetized list
       #does nothing if no matching artist is found
